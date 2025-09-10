263b9e36be5a6af5a80c9a316305810d94c3f0df
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2025-09-10 22:35:23 +0200

    Item buy system

--- Tiny Train Tales/Assets/Scripts/Cargo/CargoItem.cs
+++ Tiny Train Tales/Assets/Scripts/Cargo/CargoItem.cs
@@ -6,9 +6,19 @@ public class CargoItem : MonoBehaviour
 {
     [SerializeField] Image itemIcon;
     [SerializeField] TextMeshProUGUI itemCountText;
+    [SerializeField] GameObject[] cityUI;
+    [SerializeField] GameObject[] trainUI;
 
     int itemCount;
     string itemName;
+    bool isInCity = true;
+
+    CargoManager cargoManager;
+
+    void Awake()
+    {
+        cargoManager = FindObjectOfType<CargoManager>();
+    }
 
     public void SetItemIcon(Sprite itemSprite)
     {
@@ -26,6 +36,36 @@ public class CargoItem : MonoBehaviour
         itemCountText.text = itemCount.ToString();
     }
 
+    public void BuyItem()
+    {
+        cargoManager.AddCargo(gameObject);
+    }
+
+    public void AddCount(int count)
+    {
+        itemCount += count;
+    }
+
+    public void TurnOffBuyOption()
+    {
+
+    }
+
+    public void ChangeUI()
+    {
+        for (int i = 0; i < cityUI.Length; i++)
+        {
+            cityUI[i].SetActive(isInCity);
+        }
+
+        for (int i = 0; i < trainUI.Length; i++)
+        {
+            trainUI[i].SetActive(isInCity);
+        }
+
+        isInCity = !isInCity;   
+    }
+
     public Image GetItemIcon()
     {
         return itemIcon;
@@ -39,5 +79,10 @@ public class CargoItem : MonoBehaviour
     public string GetItemName()
     {
         return itemName;
+    }
+
+    public int GetItemCount()
+    {
+        return itemCount;
     }
 }
--- Tiny Train Tales/Assets/Scripts/Cargo/CargoManager.cs
+++ Tiny Train Tales/Assets/Scripts/Cargo/CargoManager.cs
@@ -9,17 +9,17 @@ public class CargoManager : MonoBehaviour
     [SerializeField] int minCityCargoAmount;
     [SerializeField] int maxCityCargoAmount;
     [Header("Cargo Intiate")]
-    [SerializeField] Sprite[] cargoTypesSprites;
-    [SerializeField] string[] cargoTypesNames;
+    [SerializeField] Sprite[] cargoItemsSprites;
+    [SerializeField] string[] cargoItemsNames;
     [Space]
     [SerializeField] GameObject cargoItemPrefab;
     [SerializeField] int maxCargoCount;
 
     int currentCargoAmount;
 
-    List<GameObject> cargoTypes = new List<GameObject>();
+    List<GameObject> cargoItems = new List<GameObject>();
 
-    public void AddCargo()
+    public void AddCargo(GameObject newItem)
     {
         currentCargoAmount++;
         if (currentCargoAmount > maxCargoCount)
@@ -29,11 +29,11 @@ public class CargoManager : MonoBehaviour
 
         bool hasItem = false;
         int index = 0;
-        for (int i = 0; i < cargoTypes.Count; i++)
+        for (int i = 0; i < cargoItems.Count; i++)
         {
-            for (int ii = 0; ii < cargoTypesSprites.Length; ii++)
+            for (int ii = 0; ii < cargoItemsSprites.Length; ii++)
             {
-                if (cargoTypes[i].GetComponent<CargoItem>().GetItemIcon().sprite == cargoTypesSprites[ii])
+                if (cargoItems[i].GetComponent<CargoItem>().GetItemIcon().sprite == cargoItemsSprites[ii])
                 {
                     hasItem = true;
                     index = i;
@@ -45,13 +45,17 @@ public class CargoManager : MonoBehaviour
             hasItem = false;
         }
 
-        if (!hasItem)
+        if (hasItem)
         {
-            //Add object that city created           
-        }
-        else
-        {
-            //Has Index
+            CargoItem newItemScript = newItem.GetComponent<CargoItem>();
+
+            cargoItems[index].GetComponent<CargoItem>().AddCount(1);
+            newItemScript.AddCount(-1);
+
+            if (newItemScript.GetItemCount() <= 0)
+            {
+                newItemScript.TurnOffBuyOption();
+            }
         }
     }
 
@@ -60,9 +64,9 @@ public class CargoManager : MonoBehaviour
         GameObject cargoItem = Instantiate(cargoItemPrefab);
         CargoItem cargoItemScript = cargoItem.GetComponent<CargoItem>();
 
-        int randomIndex = Random.Range(0, cargoTypesSprites.Length);
-        cargoItemScript.SetItemIcon(cargoTypesSprites[randomIndex]);
-        cargoItemScript.SetItemName(cargoTypesNames[randomIndex] + " " + cityName);
+        int randomIndex = Random.Range(0, cargoItemsSprites.Length);
+        cargoItemScript.SetItemIcon(cargoItemsSprites[randomIndex]);
+        cargoItemScript.SetItemName(cargoItemsNames[randomIndex] + " " + cityName);
 
         int randomCount = Random.Range(minSpawnCount, maxSpawnCount + 1);
         cargoItemScript.SetItemCount(randomCount);
