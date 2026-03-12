e3e14abc649a3204d07fbf493adbb81720f86280
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2026-03-12 12:59:05 +0100

    Basic day night cycle where the sky changes sprites and the background changes color

--- Tiny Train Tales/Assets/Scripts/Background/BlockMovement.cs
+++ Tiny Train Tales/Assets/Scripts/Background/BlockMovement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using static DayNightCycle;
 
 public class BlockMovement : MonoBehaviour
 {
@@ -7,22 +9,73 @@ public class BlockMovement : MonoBehaviour
     [SerializeField] int currentBlockNumber;
     [SerializeField] float spawnOffset;
 
+    [Header("Sky Block Objects (Block 4 only)")]
+    [SerializeField] GameObject morningObject;
+    [SerializeField] GameObject dayObject;
+    [SerializeField] GameObject eveningObject;
+    [SerializeField] GameObject nightObject;
+
+    [Header("Non-Sky Block Tinting")]
+    [SerializeField] SpriteRenderer[] spriteRenderers;
+
     float speed;
+    ColorBlock[] originalColors;
+    bool originalColorsInitialized;
+    DayNightCycle.TimeOfDay lastTimeOfDay = (DayNightCycle.TimeOfDay)(-1);
 
     Rigidbody2D myRigidbody;
     Train train;
     BackgroundGenerator backgroundGenerator;
+    DayNightCycle dayNightCycle;
 
     void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         train = FindObjectOfType<Train>();
         backgroundGenerator = FindObjectOfType<BackgroundGenerator>();
+        dayNightCycle = FindObjectOfType<DayNightCycle>();
     }
 
     void Start()
     {
+        if (!originalColorsInitialized)
+        {
+            originalColorsInitialized = true;
+            originalColors = new ColorBlock[spriteRenderers.Length];
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                if (spriteRenderers[i] != null)
+                {
+                    ColorBlock cb = ColorBlock.defaultColorBlock;
+                    cb.normalColor = spriteRenderers[i].color;
+                    originalColors[i] = cb;
+                }
+            }
+        }
+
         LoadSpeed();
+        DayNightEvent();
+    }
+
+    public void SetOriginalColor(ColorBlock[] spawnParentOriginalColors)
+    {
+        originalColorsInitialized = true;
+        originalColors = spawnParentOriginalColors;
+        DayNightCycle.TimeOfDay timeOfDay = dayNightCycle.currentTime;
+        lastTimeOfDay = timeOfDay;
+
+        if (spriteRenderers.Length > 0)
+        {
+            Color blockColor = dayNightCycle.GetTimeColor(timeOfDay);
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                if (spriteRenderers[i] != null)
+                {
+                    spriteRenderers[i].color = originalColors[i].normalColor;
+                    spriteRenderers[i].color *= blockColor;
+                }
+            }
+        }
     }
 
     void LoadSpeed()
@@ -32,6 +85,38 @@ public class BlockMovement : MonoBehaviour
             train.LoadTrain();
             speed = PlayerPrefs.GetFloat("Speed");
             myRigidbody.velocity = new Vector2(-speed * speedOffset, 0f);
+        }
+    }
+
+    void Update()
+    {
+        DayNightEvent();
+    }
+
+    void DayNightEvent()
+    {
+        DayNightCycle.TimeOfDay timeOfDay = dayNightCycle.currentTime;
+        if (timeOfDay == lastTimeOfDay) return;
+        lastTimeOfDay = timeOfDay;
+
+        if (currentBlockNumber == 3)
+        {
+            if (morningObject != null) morningObject.SetActive(timeOfDay == DayNightCycle.TimeOfDay.Morning);
+            if (dayObject != null)     dayObject.SetActive(timeOfDay == DayNightCycle.TimeOfDay.Day);
+            if (eveningObject != null) eveningObject.SetActive(timeOfDay == DayNightCycle.TimeOfDay.Evening);
+            if (nightObject != null)   nightObject.SetActive(timeOfDay == DayNightCycle.TimeOfDay.Night);
+        }
+        else if (spriteRenderers.Length > 0)
+        {
+            Color blockColor = dayNightCycle.GetTimeColor(timeOfDay);
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                if (spriteRenderers[i] != null)
+                {
+                    spriteRenderers[i].color = originalColors[i].normalColor;
+                    spriteRenderers[i].color *= blockColor;
+                }
+            }
         }
     }
 
@@ -45,7 +130,10 @@ public class BlockMovement : MonoBehaviour
     {
         if (other.name == "Train" && canSpawn)
         {
-            backgroundGenerator.SpawnBlock(transform.position.y, spawnOffset, gameObject);
+            GameObject spawned = backgroundGenerator.SpawnBlock(transform.position.y, spawnOffset, gameObject);
+            spawned.GetComponent<BlockMovement>().SetOriginalColor(originalColors);
+
+            
         }
     }
 
--- Tiny Train Tales/Assets/Scripts/Background/DayNightCycle.cs
+++ Tiny Train Tales/Assets/Scripts/Background/DayNightCycle.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    [SerializeField] float dayNightTime;
-    [SerializeField] float morningEveningTime;
+    [SerializeField] float dayNightDuration;
+    [SerializeField] float morningEveningDuration;
 
-    float currentDayNightTime;
-    float currentMorningEveningTime;
+    [Header("Time of Day Colors")]
+    [SerializeField] Color dayColor = Color.white;
+    [SerializeField] Color morningColor = new Color(0.9f, 0.85f, 0.75f, 1f);
+    [SerializeField] Color eveningColor = new Color(0.7f, 0.6f, 0.5f, 1f);
+    [SerializeField] Color nightColor = new Color(0.3f, 0.3f, 0.5f, 1f);
 
-    public DayTime currentTime;
+    float currentDayNightDuration;
+    float currentMorningEveningDuration;
 
-    public enum DayTime
+    public TimeOfDay currentTime;
+
+    public Color GetTimeColor(TimeOfDay time)
+    {
+        return time switch
+        {
+            TimeOfDay.Morning => morningColor,
+            TimeOfDay.Day     => dayColor,
+            TimeOfDay.Evening => eveningColor,
+            TimeOfDay.Night   => nightColor,
+            _                 => dayColor,
+        };
+    }
+
+    public enum TimeOfDay
     {
         Morning,
         Day,
@@ -20,12 +38,29 @@ public class DayNightCycle : MonoBehaviour
 
     void Start()
     {
-        currentDayNightTime = dayNightTime;
-        currentMorningEveningTime = morningEveningTime;
+        currentDayNightDuration = dayNightDuration;
+        currentMorningEveningDuration = morningEveningDuration;
     }
 
     void Update()
     {
-        
+        if (currentTime == TimeOfDay.Morning || currentTime == TimeOfDay.Evening)
+        {
+            currentMorningEveningDuration -= Time.deltaTime;
+            if (currentMorningEveningDuration <= 0f)
+            {
+                currentMorningEveningDuration = morningEveningDuration;
+                currentTime = currentTime == TimeOfDay.Morning ? TimeOfDay.Day : TimeOfDay.Night;
+            }
+        }
+        else
+        {
+            currentDayNightDuration -= Time.deltaTime;
+            if (currentDayNightDuration <= 0f)
+            {
+                currentDayNightDuration = dayNightDuration;
+                currentTime = currentTime == TimeOfDay.Day ? TimeOfDay.Evening : TimeOfDay.Morning;
+            }
+        }
     }
 }
